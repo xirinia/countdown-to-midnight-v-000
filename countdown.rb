f0c9d4f82107cf6df18757ee48a5c798c76b1f5f
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 02:27:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
-
-def countdown
+def countdown(number)
+  while number > 0
+    if number == 1
+    puts"1 SECOND!"
+  else
+    puts "#{number} SECONDS!"
+  end
+  number -=1
+end
+  return "HAPPY NEW YEAR!"
 end
