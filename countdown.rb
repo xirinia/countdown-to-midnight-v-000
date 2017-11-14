a6a0f2bb527b8ae77024ea8010b193005f753881
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 02:42:04 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,6 @@
 #write your code here
 def countdown(number)
-  while number >= 0
-    if number == 1
-    puts "1 SECOND!"
-  else
+  while number > 0
     puts "#{number} SECOND(S)!"
   end
   number -=1
