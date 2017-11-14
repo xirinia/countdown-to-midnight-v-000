96604e75b112ebbf5f603788ce9ca6cf0bb79f8d
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 02:30:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -10,3 +10,15 @@ def countdown(number)
 end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    if number == 1
+    puts "1 SECOND!"
+  else
+    puts "#{number} SECONDS!"
+  end
+  number -=1
+end
+  return "HAPPY NEW YEAR!"
+end
